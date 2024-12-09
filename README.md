bcb79a1a120e8639696da9a1aa5c67052f0e55d2
aigoncharov <andrey@goncharov.page>
2024-12-09 23:26:37 +0300

    Support cuda and cpu

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # sciq-generator
+
+Mistral-7B fine-tuned to produce complex questions. Optimized using a custom classifier for hidden states.
+
+## Quick start
+
+> Test only on Apple Silicon
+
+1. Install `poetry`
+2. `poetry install`
+3. `poetry run python main.py`
--- device.py
+++ device.py
@@ -1,5 +1,11 @@
 import torch
 
 # Device configuration and model loading
-device = torch.device("mps")
+device = None
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
 print(f"Using device: {device}")
